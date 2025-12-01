4850f4446a4fa90fde4868b9b6fa22bed70e102b
AlexisGerv <gervaudalexis@gmail.com>
2025-12-01 17:19:50 +0100

    ok

--- vue/VueAccueil.php
+++ vue/VueAccueil.php
@@ -1,18 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-require_once('vue/vueHeader.php');
-?>
-<h1>Accueil</h1>
-<p>testons ceci</p>
-<ul>
-    <li><a href="index.php">Accueil</a></li>
-    <li><a href="livre.php">Catalogue</a></li>
-</ul>
-<?php
-require_once('vue/vueFooter.php');
-?>
-=======
 require('vueHeader.php');
 ?>
 
@@ -21,14 +7,9 @@ require('vueHeader.php');
 <h1>Accueil</h1>
 <p>testons ceci</p>
 </body>
->>>>>>> 4c0b53737202354de7527688deb5b263a2c4f949
 
-<<<<<<< HEAD
-  
-=======
 </html>
 
 <?php
 require('vueFooter.php');
 ?>
->>>>>>> 6db58619cf4958e6e1bceae05353db866445bb9a
