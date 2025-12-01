952ee9c6fead52d096c86b25a68c1be0e22a95c3
btwNemS <fofi.simon.delporte@gmail.com>
2025-12-01 17:09:42 +0100

    caca

--- vue/VueFooter.php
+++ vue/VueFooter.php
@@ -1,2 +1,23 @@
-</body>
-</html>
+<footer class="footer-nav">
+  <nav class="footer-nav-inner">
+    <a href="index.php" class="nav-item active">
+      <i class="fa-solid fa-house"></i>
+    </a>
+
+    <a href="favoris.php" class="nav-item">
+      <i class="fa-regular fa-heart"></i>
+    </a>
+
+    <a href="nouveau.php" class="nav-item">
+      <i class="fa-solid fa-plus"></i>
+    </a>
+
+    <a href="profil.php" class="nav-item">
+      <i class="fa-regular fa-user"></i>
+    </a>
+
+    <a href="notifications.php" class="nav-item">
+      <i class="fa-regular fa-bell"></i>
+    </a>
+  </nav>
+</footer>
