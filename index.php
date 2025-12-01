974dc44a3238f67017983354c1522037c2f3f31f
glorpnork <elioharchi74@gmail.com>
2025-12-01 14:31:06 +0100

    location vers VueAccueil.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SAE301</title>
-    <link rel="stylesheet" href="style/style.css">
-    <script src="script/script.js" defer></script>
-</head>
-<body>
-    <h1>SAE301</h1>
-</body>
-</html>
+<?php
+
+header("location: vue/vueAccueil.php");
+
+?>
