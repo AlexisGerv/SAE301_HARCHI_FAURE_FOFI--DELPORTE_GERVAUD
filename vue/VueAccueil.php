3eb0c0fa6328a23dd5a9f2116c9b68e8ac7c1198
AlexisGerv <gervaudalexis@gmail.com>
2025-12-01 17:05:32 +0100

    c bon

--- vue/VueAccueil.php
+++ vue/VueAccueil.php
@@ -1,24 +1,15 @@
 <?php
 
 require_once('vue/vueHeader.php');
-
-require_once('vue/vueFooter.php');
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-  <h1>Accueil</h1>
-  <p>testons ceci</p>
-  <ul>
+?>
+<h1>Accueil</h1>
+<p>testons ceci</p>
+<ul>
     <li><a href="index.php">Accueil</a></li>
     <li><a href="livre.php">Catalogue</a></li>
-  </ul>
-</body>
+</ul>
+<?php
+require_once('vue/vueFooter.php');
+?>
 
-</html>
+  
