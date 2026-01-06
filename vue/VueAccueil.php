143064ca586465043cd1c9ba575fd89734be285e
Nafaur21000 <nael.faure@iut-dijon.u-bourgogne.fr>
2026-01-06 13:37:42 +0100

    aaaaaa

--- vue/VueAccueil.php
+++ vue/VueAccueil.php
@@ -8,6 +8,7 @@ require_once('./VueHeader.php');
     <li><a href="index.php">Accueil</a></li>
     <li><a href="livre.php">Catalogue</a></li>
 </ul>
+<p>aaaaaaaaaaaaaaaaa</p>
 <?php
 require_once('./VueFooter.php');
 ?>
