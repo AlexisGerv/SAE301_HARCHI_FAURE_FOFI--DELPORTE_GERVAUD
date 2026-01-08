48ce7e220634ceee45e9c086c393cf3acc0e59df
btwNemS <fofi.simon.delporte@gmail.com>
2026-01-08 10:20:29 +0100

    perfection de la navbar

--- vue/VueFooter.php
+++ vue/VueFooter.php
@@ -3,16 +3,20 @@
     <div class="wheel-bg"></div>
     <ul class="wheel" id="wheel">
       <li class="item">
-        <img src="../public//assets/setting.svg" alt="setting.svg" />
+        <?php include '../public/assets/icons/setting.svg'; ?>
       </li>
       <li class="item">
-        <img src="../public//assets/profile.svg" alt="profile.svg" />
+        <?php include '../public/assets/icons/profile.svg'; ?>
       </li>
       <li class="item active">
-        <img src="../public/assets/house.svg" alt="house" />
+        <?php include '../public/assets/icons/house.svg'; ?>
       </li>
-      <li class="item"><img src="../public/assets/pin.svg" alt="pin" /></li>
-      <li class="item"><img src="../public//assets/cart.svg" alt="cart.svg" /></li>
+      <li class="item">
+        <?php include '../public/assets/icons/pin.svg'; ?>
+      </li>
+      <li class="item">
+        <?php include '../public/assets/icons/cart.svg'; ?>
+      </li>
     </ul>
   </div>
   <script src="../public/script/footer-script.js" defer></script>
