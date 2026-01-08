7fac70de7fa80d16786a32196c9a150545a1cc6a
AlexisGerv <gervaudalexis@gmail.com>
2026-01-08 11:33:28 +0100

    commentaire

--- bibliotheque.sql
+++ bibliotheque.sql
@@ -1,12 +1,14 @@
 -- Table pour les utilisateurs
 
-CREATE TABLE Utilisateur ( --A modifier
+CREATE TABLE Etudiant ( --A modifier
 id INT PRIMARY KEY AUTO_INCREMENT,
 nom VARCHAR(100) NOT NULL,
 prenom VARCHAR(100) NOT NULL,
 mail_iut VARCHAR(255) NOT NULL UNIQUE,
-num_etudiant VARCHAR(50) UNIQUE, # Peut être NULL pour les bibliothécaires
-formation VARCHAR(100), # Ex: MMI, INFO-COM, GACO/GEA)
+num_etudiant VARCHAR(50) UNIQUE, 
+formation VARCHAR(100), -- Ex: MMI, INFO-COM, GACO/GEA)
+peut_emprunt boolean NOT NULL DEFAULT TRUE -- Peut emprunter des livres mais interdit si il rend trop de livre en retard ou en mauvais état
+)
 
 CREATE  TABLE Livre(
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -16,20 +18,33 @@ CREATE  TABLE Livre(
     editeur VARCHAR(100),
     annee_publication INT,
     genre VARCHAR(50),
-    collection VARCHAR(100),
+    _collection VARCHAR(100),
     nombre_pages INT,
-    resume TEXT,
-    image VARCHAR(255),
+    _resume TEXT,
+    _image VARCHAR(255),
     stock INT NOT NULL,
     disponible BOOLEAN NOT NULL DEFAULT TRUE  -- Disponible = 1, Non disponible = 0 
 )
 
 CREATE TABLE Emprunt(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_utilisateur INT NOT NULL,
+    id_etudiant INT NOT NULL,
     id_livre INT NOT NULL,
     date_emprunt DATE NOT NULL,
     date_retour DATE,
-    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id),
+    FOREIGN KEY (id_etudiant) REFERENCES Etudiant(id),
     FOREIGN KEY (id_livre) REFERENCES Livre(id)
 )
+
+ALTER TABLE Emprunt ADD FOREIGN KEY (id_etudiant) REFERENCES Etudiant(id);
+ALTER TABLE Emprunt ADD FOREIGN KEY (id_livre) REFERENCES Livre(id);
+
+CREATE TABLE bibliothécaire(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    mail_iut VARCHAR(255) NOT NULL UNIQUE,
+    num_personnel VARCHAR(50) UNIQUE,
+    _admin BOOLEAN NOT NULL DEFAULT TRUE
+)   
+
