9d38b8332a7b7a9f115135ac9fe0e0a14a5cbd45
AlexisGerv <gervaudalexis@gmail.com>
2026-01-14 22:12:05 +0100

    autoload.php

--- autoload.php
+++ autoload.php
@@ -4,22 +4,16 @@
  * Plus besoin de faire des require partout !
  */
 spl_autoload_register(function ($nom_classe) {
-    // 1. On définit le chemin vers le dossier "class"
-    // __DIR__ permet de toujours partir de l'emplacement de ce fichier
     $dossier_classe = __DIR__ . '/class/';
-
-    // 2. Liste des endroits où chercher les classes
-    // On doit lister les sous-dossiers car tes classes sont rangées
     $chemins_possibles = [
         $dossier_classe . $nom_classe . '.class.php',           // Ex: class/Livre.class.php
         $dossier_classe . 'Manager/' . $nom_classe . '.class.php' // Ex: class/Manager/ManagerUser.class.php
     ];
 
-    // 3. On teste chaque chemin
     foreach ($chemins_possibles as $fichier) {
         if (file_exists($fichier)) {
             require_once $fichier;
-            return; // On arrête dès qu'on a trouvé
+            return;
         }
     }
 });
