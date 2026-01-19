ae8bb3dd1749f290c263124b8e6332768d5cac89
AlexisGerv <gervaudalexis@gmail.com>
2026-01-19 19:40:00 +0100

    blabla

--- test_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $bdd = new PDO("mysql:host=localhost;dbname=bibliotheque", "root", "");
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connection SUCCESS";
-} catch (PDOException $e) {
-    echo "Connection FAILED: " . $e->getMessage();
-}
