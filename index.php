c1171dae4f36cc273b7af154bc3807617918c22d
Nafaur21000 <nael.faure@iut-dijon.u-bourgogne.fr>
2026-01-20 18:17:14 +0100

    rootage

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <?php
-// index.php à la racine
+session_start(); 
 require_once './autoload.php';
-require_once './modeles/connect.php'; // Définit $bdd
+require_once './modeles/connect.php';
 
-// 1. Récupération de la recherche (on utilise POST car ton formulaire est en POST)
-$recherche = isset($_POST['recherche']) ? trim(htmlspecialchars($_POST['recherche'])) : '';
+// On récupère la page demandée (par défaut 'accueil')
+$page = isset($_GET['page']) ? $_GET['page'] : 'accueil';
 
-// 2. Initialisation pour la VueAccueil.php
-$resultats = [];
-
-// 3. Si une recherche est faite, on passe par le contrôleur dédié
-if (!empty($recherche)) {
-    require_once './controleurs/recherche.php';
-}
-
-// 4. Affichage de la structure
 include './vue/VueHeader.php';
 ?>
 
 <main id="content">
-    <?php include './vue/VueAccueil.php'; ?>
+    <?php 
+    // Le routeur choisit quelle vue afficher au milieu du "sandwich"
+    switch($page) {
+        case 'connexion':
+            include './vue/VueConnexion.php';
+            break;
+        case 'livre':
+            include './vue/VueLivre.php';
+            break;
+        default:
+            include './vue/VueAccueil.php';
+            break;
+    }
+    ?>
 </main>
 
-<?php include './vue/VueFooter.php'; ?>
+<?php 
+include './vue/VueFooter.php'; 
+?>
