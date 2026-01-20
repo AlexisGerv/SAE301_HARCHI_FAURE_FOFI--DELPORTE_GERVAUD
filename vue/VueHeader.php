de8c20aff7f9b19de44580ab66ee4e0bf253f959
Nafaur21000 <nael.faure@iut-dijon.u-bourgogne.fr>
2026-01-20 20:19:03 +0100

    lien css

--- vue/VueHeader.php
+++ vue/VueHeader.php
@@ -15,11 +15,11 @@
         referrerpolicy="no-referrer" />
 
     <!-- Styles -->
-    <link rel="stylesheet" href="../public/style/header-style.css">
-    <link rel="stylesheet" href="../public/style/livre-style.css">
-    <link rel="stylesheet" href="../public/style/accueil-style.css">
-    <link rel="stylesheet" href="../public/style/footer-style.css">
-    <script src="../public/script/footer-script.js" defer></script>
+    <link rel="stylesheet" href="public/style/header-style.css">
+    <link rel="stylesheet" href="public/style/livre-style.css">
+    <link rel="stylesheet" href="public/style/accueil-style.css">
+    <link rel="stylesheet" href="public/style/footer-style.css">
+    <script src="public/script/footer-script.js" defer></script>
 
 </head>
 
