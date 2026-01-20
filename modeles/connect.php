c56d51407561a8c07dbd649476d1fa527c32f62f
Nafaur21000 <nael.faure@iut-dijon.u-bourgogne.fr>
2026-01-20 10:55:10 +0100

    cacaa Please enter the commit message for your changes. Lines starting

--- modeles/connect.php
+++ modeles/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $bdd = new PDO("mysql:host=localhost;dbname=bibliothèque", "root", "");
+    $bdd = new PDO("mysql:host=localhost;dbname=bibliotheque", "root", "");
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
